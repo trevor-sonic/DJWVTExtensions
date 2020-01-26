44fe7cc5218ca62ed7cce75b7247303ffe06535c
Trevor <trevor.d@beydag.com>
2020-01-26 03:44:19 +0000

    String UI size added.

--- Sources/DJWVTExtensions/StringExt.swift
+++ Sources/DJWVTExtensions/StringExt.swift
@@ -18,3 +18,82 @@ public extension String {
         return date!
     }
 }
+
+public extension String {
+    
+    mutating func trim(){
+        self =  self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+    mutating func fillWithIfItsEmpty(_ fillStr:String){
+        if self.count < 1 {
+            self =  fillStr
+        }
+    }
+}
+
+// MARK: - UI + String
+
+extension String {
+    /// Returns pixel sizes of text
+    func sizeAsPixel(usedFont:UIFont)->CGSize{
+        let nsString = (self + "<<=END" as NSString)
+        let range: NSRange = nsString.range(of: "<<=END")
+        let prefix = nsString.substring(to: range.location)
+        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: usedFont])
+        return size
+    }
+    /// Returns pixel position of searched text
+    func position(of textToFind:String, usedFont:UIFont)->CGFloat{
+        let nsString = (self as NSString)
+        
+        let range: NSRange = nsString.range(of: textToFind)
+        let prefix = nsString.substring(to: range.location)
+        
+        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: usedFont])
+        let pos = CGPoint(x: size.width , y: 0)
+
+        return pos.x
+    }
+    
+    /// Returns pixel position of index
+    func position(of index:Int, usedFont:UIFont)->CGFloat{
+        let nsString = (self as NSString)
+        
+        let range: NSRange = NSMakeRange(index, 1)
+        let prefix = nsString.substring(to: range.location)
+        
+        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: usedFont])
+        let pos = CGPoint(x: size.width , y: 0)
+
+        return pos.x
+    }
+}
+
+/// substring helper
+extension String {
+
+  var length: Int {
+    return count
+  }
+
+  subscript (i: Int) -> String {
+    return self[i ..< i + 1]
+  }
+
+  func substring(fromIndex: Int) -> String {
+    return self[min(fromIndex, length) ..< length]
+  }
+
+  func substring(toIndex: Int) -> String {
+    return self[0 ..< max(0, toIndex)]
+  }
+
+  subscript (r: Range<Int>) -> String {
+    let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                        upper: min(length, max(0, r.upperBound))))
+    let start = index(startIndex, offsetBy: range.lowerBound)
+    let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+    return String(self[start ..< end])
+  }
+
+}
